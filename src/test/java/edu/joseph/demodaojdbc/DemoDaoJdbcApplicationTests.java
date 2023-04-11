c4f2c1e9335d9748b73cc4ee5e23907618db612a
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2023-04-11 18:15:48 -0300

    Terceiro commit: testes basicos da classe Department

--- src/test/java/edu/joseph/demodaojdbc/DemoDaoJdbcApplicationTests.java
+++ src/test/java/edu/joseph/demodaojdbc/DemoDaoJdbcApplicationTests.java
@@ -1,13 +1,23 @@
 package edu.joseph.demodaojdbc;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import edu.joseph.model.Department;
 
-@SpringBootTest
+
 class DemoDaoJdbcApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void myDepartment() {
+		var obj = new Department(1, "Books");
+		System.out.println(obj.toString());
 	}
-
+	
+	@Test
+	void myDepartmentEquals() {
+		var obj = new Department(1, "Books");
+		var obj1 = new Department(2, "Eletronics");
+		boolean comparison = obj.equals(obj1) ? true : false; 
+		System.out.println(comparison);
+		
+	}
 }
