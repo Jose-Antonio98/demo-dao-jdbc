d1b99ff920186cc42437b0d8157458767d0834f5
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2023-04-11 17:30:28 -0300

    Primeiro commit: criação do escopo do projeto

---
+++ src/main/java/edu/joseph/db/DbIntegrityException.java
@@ -0,0 +1,10 @@
+package edu.joseph.db;
+
+public class DbIntegrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DbIntegrityException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/test/java/edu/joseph/demodaojdbc/DemoDaoJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.joseph.demodaojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDaoJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
